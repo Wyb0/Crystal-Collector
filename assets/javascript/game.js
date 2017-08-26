5409abd8d22202799ae2fa7e17638ffca00595a5
Andrew Wybo <Andrew Wybo>
2017-08-26 00:00:32 -0400

    added html classes, and completed js random num gen

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,43 @@
+//set variables
+var wins = 0;
+var losses = 0;
+var score = 0;
+
 $(document).ready(function() {
+    //generate random number for guessing
+    var randomNum = Math.floor((Math.random()) * 101 + 19);
+    $(".randomnumber").append(randomNum);
+    console.log(randomNum);
 
+    //generate random number for crystals
+    var red = Math.floor((Math.random()) * 11 + 1);
+    $(".redcrystal").val(red);
+    console.log(red);
+    var blue = Math.floor((Math.random()) * 11 + 1);
+    $(".bluecrystal").val(blue);
+    console.log(blue);
+    var green = Math.floor((Math.random()) * 11 + 1);
+    $(".greencrystal").val(green);
+    console.log(green);
+    var purple = Math.floor((Math.random()) * 11 + 1);
+    $(".purplecrystal").val(purple);
+    console.log(purple);
 
+    $(".redcrystal").on("click", function () {
+    
+    }
+    $(".bluecrystal").on("click", function () {
+        
+    }
+    $(".greencrystal").on("click", function () {
+        
+    }
+    $(".purplecrystal").on("click", function () {
+        
+    }
 
+});
 
-
-
-
-
-
-
-
-
-
-
-}
+//for loop for crystal random number
+//function crystal (color) {
+    //color = Math.floor((Math.random()) * 11 + 1);
