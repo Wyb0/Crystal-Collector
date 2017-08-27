63d6d14d9af9832639b4c29babf66302c8807151
Andrew Wybo <Andrew Wybo>
2017-08-27 16:50:55 -0400

    Corrected win/los, now working and tallying

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,12 +4,10 @@
  var score = 0;
 
 $(document).ready(function() {
-    
     //generate random number for guessing
     var randomNum = Math.floor((Math.random()) * 101 + 19);
         $(".randomnumber").append(randomNum);
         console.log(randomNum);
-
     //generate random number for crystals
         //note *couldn't use two classes in html for crystals
     var red = Math.floor((Math.random()) * 11 + 1);
@@ -28,41 +26,39 @@ $(document).ready(function() {
         $(".purplecrystal").val(purple);
         var purpleadd = parseInt($(purple));
         console.log(purple);
-
-
     //generate on click event for crystals
-        //note *couldn't replace same line in html, had to seperate
-    
-        
     $(".redcrystal").on("click", function () {
         var redfinal = score += red;
         $(".totalscore").html(redfinal);
+        checkwin (score);
     })
     $(".bluecrystal").on("click", function () {
         var bluefinal = score += blue;
         $(".totalscore").html(bluefinal);
+        checkwin (score);
     })
     $(".greencrystal").on("click", function () {
         var greenfinal = score += green;
         $(".totalscore").html(greenfinal);
+        checkwin (score);
     })
     $(".purplecrystal").on("click", function () {
         var purplefinal = score += purple;
         $(".totalscore").html(purplefinal);
-    })
-    
-
-    console.log(score)
-    console.log(randomNum)
-    //if else statements to define win-loss
+        checkwin (score);
+        console.log(score)
+    })  
+//if else statements to define win-loss
+   function checkwin () {
     if (score == randomNum) {
         wins++;
-        $(".winstally").html(wins);
-    }
+        $(".winstally").html("Wins: " + wins);
+        };
     if (score > randomNum) {
         losses++;
-        $(".lossestally").html(losses);
-    }
+        $(".lossestally").html("Losses: " + losses);
+        };
+    };
 
 });
 
